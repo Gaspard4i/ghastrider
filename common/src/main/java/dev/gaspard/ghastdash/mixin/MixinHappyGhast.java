b895d7974f80b6c88ec75e97948f60cc0c4bb810
Gaspard4i <catry.gaspard@gmail.com>
2026-04-15 19:36:52 +0200

    feat: ice speed effects on Happy Ghast + debug function
    - Add ice feeding mechanic: ice (Speed I), packed ice (Speed II),
      blue ice (Speed III), powder snow bucket (Speed I + empties bucket)
    - Same ice type stacks duration (+10s each), different type resets to 10s
    - Add MixinHappyGhast to intercept mob interaction
    - Replace test functions with single debug function that spawns
      a NoAI/Invulnerable Happy Ghast and gives all test items

---
+++ common/src/main/java/dev/gaspard/ghastdash/mixin/MixinHappyGhast.java
@@ -0,0 +1,26 @@
+package dev.gaspard.ghastdash.mixin;
+
+import dev.gaspard.ghastdash.IceEffectHelper;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.animal.happyghast.HappyGhast;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(HappyGhast.class)
+public class MixinHappyGhast {
+
+    @Inject(method = "mobInteract", at = @At("HEAD"), cancellable = true)
+    private void ghastdash$onInteract(Player player, InteractionHand hand, CallbackInfoReturnable<InteractionResult> cir) {
+        HappyGhast self = (HappyGhast) (Object) this;
+        ItemStack stack = player.getItemInHand(hand);
+        InteractionResult result = IceEffectHelper.handleIceInteraction(self, player, hand, stack);
+        if (result != null) {
+            cir.setReturnValue(result);
+        }
+    }
+}
---
+++ common/src/main/resources/data/ghastdash/function/debug.mcfunction
@@ -0,0 +1,16 @@
+difficulty peaceful
+gamerule doDaylightCycle false
+gamerule doMobSpawning false
+gamerule doWeatherCycle false
+time set day
+weather clear
+
+kill @e[type=minecraft:happy_ghast]
+summon minecraft:happy_ghast ~ ~2 ~2 {NoAI:1b,Invulnerable:1b}
+
+give @s minecraft:white_harness 1
+give @s minecraft:ice 64
+give @s minecraft:packed_ice 64
+give @s minecraft:blue_ice 64
+give @s minecraft:powder_snow_bucket 1
+give @s minecraft:snowball 64
